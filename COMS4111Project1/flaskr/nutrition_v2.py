a10503322dfb046dccb4d98c5b26af4d05f3c0bf
Kezia Setokusumo <111642763+keziasetokusumo@users.noreply.github.com>
2024-11-12 13:21:10 -0500

    Update nutrition_v2.py

--- COMS4111Project1/flaskr/nutrition_v2.py
+++ COMS4111Project1/flaskr/nutrition_v2.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .db import get_db
 from psycopg2.extras import DictCursor
-from datetime import datetime
 
 bp = Blueprint('nutrition', __name__, url_prefix='/nutrition')
 
@@ -22,35 +21,34 @@ def index():
 @bp.route('/add', methods=('GET', 'POST'))
 def add_nutrition():
     if request.method == 'POST':
-        member_name = request.form['member_name']
-        nutrition_name = request.form['nutrition_name']
+        name = request.form['name']
         calories = request.form['calories']
         proteins = request.form['proteins']
         carbohydrates = request.form['carbohydrates']
-        
+        member_name = request.form['member_name']
+
         db = get_db()
         with db.cursor(cursor_factory=DictCursor) as cursor:
-            cursor.execute("SELECT member_id FROM Member WHERE name = %s;", (member_name,))
+            cursor.execute("SELECT member_id FROM Member WHERE name = %s", (member_name,))
             member = cursor.fetchone()
-            if member is None:
-                cursor.execute("INSERT INTO Member (name) VALUES (%s) RETURNING member_id;", (member_name,))
-                member_id = cursor.fetchone()['member_id']
-            else:
-                member_id = member['member_id']
             
+            if member is None:
+                flash('Error: Member does not exist. Please enter a valid member name.', 'error')
+                return redirect(url_for('nutrition.add_nutrition'))
+
+            member_id = member['member_id']
+                
             cursor.execute("""
-                INSERT INTO Nutrition (name, calories, proteins, carbohydrates, created_at)
-                VALUES (%s, %s, %s, %s, %s) RETURNING nutrition_id;
-            """, (nutrition_name, calories, proteins, carbohydrates, datetime.now()))
+                INSERT INTO Nutrition (name, calories, proteins, carbohydrates)
+                VALUES (%s, %s, %s, %s) RETURNING nutrition_id
+            """, (name, calories, proteins, carbohydrates))
             nutrition_id = cursor.fetchone()['nutrition_id']
-        
-            cursor.execute("""
-                INSERT INTO Tracks (member_id, nutrition_id) VALUES (%s, %s);
-            """, (member_id, nutrition_id))
+
+            cursor.execute("INSERT INTO Tracks (member_id, nutrition_id) VALUES (%s, %s)", (member_id, nutrition_id))
             
             db.commit()
-            
-            flash("Nutrition information added successfully!", "success")
-            return redirect(url_for('nutrition.index'))
-    
+
+        flash('Nutrition information added successfully!', 'success')
+        return redirect(url_for('nutrition.index'))
+
     return render_template('nutrition/add.html')
